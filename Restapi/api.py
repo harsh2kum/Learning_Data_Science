8983b289e2da3ebba7281c89ea8b8e23b600ad92
harsh2kum <hk1827523@gmail.com>
2025-11-11 23:36:32 +0530

    Add api file

---
+++ Restapi/api.py
@@ -0,0 +1,9 @@
+# # removed unused 'requests' import
+# import requests
+# import json
+# #make a GET requests to a public API
+# response = request.get("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow")
+
+# print(response)
+# # Convert the response to JSON
+# # data  = response.json()
